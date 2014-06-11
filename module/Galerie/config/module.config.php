d68ed78fadfbf68ec3d73a5d06bc9f9550606271
mbongo <pat@mbongoproject.com>
2014-06-11 17:34:08 +0200

    template_map setting on module.config.php

--- module/Galerie/config/module.config.php
+++ module/Galerie/config/module.config.php
@@ -7,6 +7,13 @@ return array(
 
       ), 
 'view_manager' => array(
+'template_map' => array(
+   'galerie/index/index' => _DIR_.'/../view/galerie/index/index.phtml',
+   'galerie/index/edit' => _DIR_.'/../view/galerie/index/edit.phtml',
+   'galerie/index/del' => _DIR_.'/../view/galerie/index/del.phtml',
+   'galerie/index/view' => _DIR_.'/../view/galerie/index/view.phtml',
+
+ ),
     'template_path_stack' => array(
         'galerie' => _DIR_.'/../view',
        ),
