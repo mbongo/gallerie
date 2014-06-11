20b9171a26b294ebf2e7562a93f6d4c7a6ad4208
mbongo <pat@mbongoproject.com>
2014-06-11 15:50:15 +0200

    ffdd0bd

--- module/Galerie/Module.php
+++ module/Galerie/Module.php
@@ -1,7 +1,36 @@
 <?php
 namespace Galerie
 
-class Module{
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+class Module implements AutoloaderProviderInterface {
+
+   public function getAutoloaderConfig(){ 
+  //chargement automatique des classes php
+     
+    return array(
+      'Zend\Loader\ClassMapAutoloader' => array(
+          _DIR_.'/autoload_classmap.php',
+     ),
+ );
+
+
+ 
+
+
+
+   
+
+   return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+
+}
 
 
 }
---
+++ module/Galerie/autoload_classmap.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+'Galerie\Controller\IndexController' => _DIR_.'/src/Galerie/Controller/IndexController.php',
+
+     );
