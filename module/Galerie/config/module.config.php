80314b3f7e9b920426902169aa99066242eb69fb
mbongo <pat@mbongoproject.com>
2014-06-15 10:54:42 +0200

    adding all routes

--- module/Galerie/config/module.config.php
+++ module/Galerie/config/module.config.php
@@ -5,7 +5,8 @@ return array(
        'Galerie\Controllers\Index' => 'Galerie\Controllers\IndexController'
         ),
 
-      ), 
+      ), //end of controller
+      
 'view_manager' => array(
 'template_map' => array(
    'galerie/index/index' => _DIR_.'/../view/galerie/index/index.phtml',
@@ -17,12 +18,91 @@ return array(
     'template_path_stack' => array(
         'galerie' => _DIR_.'/../view',
        ),
-     ),
+     ),//end of view manager
+		
 'router'=> array(
     'routes' => array(
+    		
       'galerie' => array(
-       ),
-     ),
-  ),
+           'type'    => 'literal',
+           'options' => array(
+               'route'    => '/galeries',
+               'defaults' => array(
+                '_NAMESPACE_'   => 'Galerie\Controller',
+                'controller'    => 'Index',
+                'action'        => 'index',
+                'verb'          => 'get'  ,
+                'may_terminate' => 'true' ,
 
+               ),
+
+           ),
+
+       ),//end of route index 
+       
+    	'add' => array(
+    				'type'    => 'literal',
+    				'options' => array(
+    						'route'    => '/ajouter',
+    						'defaults' => array(
+    								'action'        => 'edit',
+    						),
+    					),
+    								
+    				'verb'          => 'get,post' ,
+    								
+    		
+    	),//en of route add
+    	
+    	'edit' => array(
+    				'type'    => 'Segment',
+    				'options' => array(
+    						'route'    => '/editer/:id',
+    						'constraints' => array(
+    								'id'  => '[1-9][0-9]*',
+    						),
+    						'defaults' => array(
+    								'action'  => 'edit',
+    						),
+    				),
+    		
+    				'verb'          => 'get,post' ,
+    		
+     ),// end of route edit 
+     
+    'del' => array(
+    				'type'    => 'Segment',
+    				'options' => array(
+    						'route'    => '/supprimer/:id',
+    						'constraints' => array(
+    								'id'  => '[1-9][0-9]*',
+    						),
+    						'defaults' => array(
+    								'action'  => 'del',
+    						),
+    				),
+    		
+    				'verb'          => 'get,post' ,
+    		
+    		),// end of route del
+    		
+    		'view' => array(
+    				'type'    => 'Segment',
+    				'options' => array(
+    						'route'    => '/voir/:id',
+    						'constraints' => array(
+    								'id'  => '[1-9][0-9]*',
+    						),
+    						'defaults' => array(
+    								'action'  => 'view',
+    						),
+    				),
+    		
+    				'verb'          => 'get,post' ,
+    		
+    		),// end of route view
+  ),//end of all routes 
+
+),//end of router 
+		
 );
