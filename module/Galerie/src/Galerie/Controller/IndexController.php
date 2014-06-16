329652a84f532a1e37c8206aaf19a88a54f63afb
mbongo <pat@mbongoproject.com>
2014-06-16 10:59:44 +0200

    adding IndexController

---
+++ module/Galerie/src/Galerie/Controller/IndexController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Galerie\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return array();
+    }
+
+
+    public function editAction()
+    {
+        return array();
+    }
+
+
+    public function delAction()
+    {
+        return array();
+    }
+
+    public function viewAction()
+    {
+        return array();
+    }
+
+
+}
