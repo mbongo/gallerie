353c88d2dce83fe90ef187d9656de00579692757
mbongo <pat@mbongoproject.com>
2014-06-16 11:43:46 +0200

    073c2ba

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,7 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application',
+        'Application','Galerie',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
@@ -11,16 +11,21 @@ return array(
         // If a string key is provided, the listener will consider that a module
         // namespace, the value of that key the specific path to that module's
         // Module class.
-        'module_paths' => array(
-            './module',
-            './vendor',
-        ),
+        //'module_paths' => array( 
+         //   './module',
+          //  './vendor',
+        //),
 
         // An array of paths from which to glob configuration files after
         // modules are loaded. These effectively override configuration
         // provided by modules themselves. Paths may use GLOB_BRACE notation.
         'config_glob_paths' => array(
             'config/autoload/{,*.}{global,local}.php',
+        ),
+
+        'module_paths' => array( 
+           './module',
+            './vendor',
         ),
 
         // Whether or not to enable a configuration cache.
